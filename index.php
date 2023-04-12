e78f53552dbb3912f0b80e46e4c0840b569b3469
bartkastermans <187614@novacollege.nl>
2023-04-12 14:17:26 +0200

    derde commit: prs pagina met side menu

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<style>
+    .displayprs {
+        position: relative;
+        padding-left: 10px;
+        left: 0px;
+        bottom: -35px;
+    }
+    .displayprsside .displayprs{
+        left: 135px;
+    }
+    .side-menu {
+        position: fixed;
+        top: 0;
+        left: -200px;
+        width: 150px;
+        height: 100%;
+        background-color: #f2f2f2;
+        transition: left 0.2s ease-in-out;
+    }
+
+    .menu-open .side-menu {
+        left: 0;
+    }
+
+    .menu-toggle {
+        position: fixed;
+        padding: 10px;
+        left: 20px;
+        top: 5px;
+        background-color: #f2f2f2;
+        border: none;
+        cursor: pointer;
+    }
+    .menu-toggleleft .menu-toggle{
+        left: 155px;
+    }
+</style>
+
 <?
 $servername = "localhost";
 $username = "root";
@@ -12,18 +50,10 @@ $sure = $_GET["sure"];
 $add = $_GET["add"];
 $benchpress = $_GET["bench_press"];
 $gebruikersnaam = $_GET["gebruikersnaam"];
-$email = $_GET["email"];
-$telefoonnummer = $_GET["telefoonnummer"];
 $edit = $_GET["edit"];
-$adres = $_GET["adres"];
-$postcode = $_GET["postcode"];
-$woonplaats = $_GET["woonplaats"];
 $land = $_GET["land"];
 $pasaan = $_GET["pasaan"];
 $submit = $_GET["submit"];
-
-include 'test_input.php';
-include 'navigatie.php';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -31,13 +61,14 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
+/*
 if ($add == 1) {
+
     ?>
     <b>Voeg een gast toe.</b>
     <form action="?" method="get">
         <input type="hidden" name="add" value="1">
-        Voornaam: <input type="text" name="bench_press" value="<?php echo "$benchpress"; ?>"> <br>
+        Oefening Naam: <input type="text" name="oefening_naam" value="<?php echo "$benchpress"; ?>"> <br>
         <input type="submit" style="color:Green" name="submit" value="Toevoegen">
     </form>
     <?php
@@ -53,7 +84,7 @@ if ($add == 1) {
         }
     }
 }
-
+*/
 if ($edit == 1) {
 
     if ($pasaan == 1) {
@@ -79,55 +110,63 @@ if ($edit == 1) {
     }
 }
 
-$sqldelete = "DELETE FROM gebruikers WHERE id = $id";
-if ($delete == 1) {
-    if ($sure == 1) {
-        $result = $conn->query($sqldelete);
-        echo "Het record met id $id is succesvol verwijderd uit de database. <br><br>";
-    } else {
-        echo 'Weet je het zeker domme os? <table> <tr> <td><form action="?" method="get"><input type="hidden" name="sure" value="1"><input type="hidden" name="delete" value="1"><input type="hidden" name="id" value="' . $id . '"><input type="submit" style="color:Green" value="Ja"></form></td> <td><form action="?" method="get"><input type="submit" style="color:Red" value="Nee"></form></td></tr></table><br><br>';
-    }
-}
 
 $sql = "SELECT * FROM gebruikers ORDER BY id";
 $result = $conn->query($sql);
 
-echo('<b>Deze info komt uit de database:<br></b>');
-
 if ($result->num_rows > 0) {
 ?>
-<table>
-    <tr style="background-color:#cccccc;">
-        <th>ID</th>
-        <th>GebruikersNaam</th>
-        <th>Bench Press</th>
-        <th>&nbsp;</th>
-    </tr>
-    <?php
-    while($row = $result->fetch_assoc()) {
-        // echo "id: " . $row["id"]. " - Name: " . $row["bench_press"]. " " . $row["lastname"]. "<br><br>";
+<div class="menu-toggle">
+    <button class="menu-toggle" >Open Menu</button>
+</div>
 
-        if ($teller == 2) {
-            $kleur = "cccccc";
-            $teller = $teller - 1;
-        } else {
-            $kleur = "ffffff";
-            $teller = $teller + 1;
+<div class="side-menu">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="prs.php">PRs</a></li>
+        <li><a href="kalender.php">Kalender</a></li>
+    </ul>
+</div>
+
+<div class="displayprs" >
+    <table style="width: 350px; height: 120px;" >
+        <tr style="background-color:#cccccc;">
+            <th>ID</th>
+            <th>GebruikersNaam</th>
+            <th>Bench Press</th>
+        </tr>
+        <?php
+        while($row = $result->fetch_assoc()) {
+
+            if ($teller == 2) {
+                $kleur = "cccccc";
+                $teller = $teller - 1;
+            } else {
+                $kleur = "ffffff";
+                $teller = $teller + 1;
+            }
+
+            echo('<tr style="background-color:#' . $kleur . ';"> <td>' . $row["id"] . '</td> 
+    <td><a href="index.php?edit=1&id=' . $row["id"] . '">' . $row["gebruikersnaam"] . '</a></td> 
+    <td><a href="index.php?edit=1&id=' . $row["id"] . '">' . $row["bench_press"] . '</a></td> 
+    <td><form action="?" method="get"><input type="hidden" name="delete" value="1"><input type="hidden" name="id" value="' . $row["id"] . '"></form></td> </tr>');
         }
-
-        echo('<tr style="background-color:#' . $kleur . ';"> <td>' . $row["id"] . '</td> 
-<td><a href="index.php?edit=1&id=' . $row["id"] . '">' . $row["gebruikersnaam"] . '</a></td> 
-<td><a href="index.php?edit=1&id=' . $row["id"] . '">' . $row["bench_press"] . '</a></td> 
-<td><form action="?" method="get"><input type="hidden" name="delete" value="1"><input type="hidden" name="id" value="' . $row["id"] . '"><input type="submit" style="color:red" value="Delete"></form></td> </tr>');
-    }
-    echo "</table>";
+        echo "</table>";
+        echo '<a href="index.php?add=1">Record toevoegen</a>';
+        echo "</div>";
     } else {
         echo "0 results";
     }
 
-    echo '<a href="index.php?add=1">Record toevoegen</a>';
 
     $conn->close();
-
-
     ?>
+
+        <script>
+            document.querySelector('.menu-toggle').addEventListener('click', function() {
+                document.body.classList.toggle('menu-open');
+                document.body.classList.toggle('displayprsside');
+                document.body.classList.toggle('menu-toggleleft');
+            });
+
+        </script>
